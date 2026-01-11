05a10e03f07d0594cb7a024c1e830ea663c97cec
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-11 23:10:01 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-# Khalid Husain - Ultimate Framework Setup
+echo -e "\e[1;32m[*] Installing Khalid All-in-One OSINT Framework...\e[0m"
 
-echo -e "\e[1;32m[*] 6-Layer OSINT Framework Install ho raha hai...\e[0m"
+# 1. Install Networking & Tor
+sudo apt update
+sudo apt install tor proxychains4 whois dnsutils python3-pip -y
 
-# 1. Background Services
-sudo apt update && sudo apt install tor whois dnsutils python3-pip -y
-sudo service tor start
+# 2. Configure Proxychains (Silent Setup)
+sudo sed -i 's/strict_chain/#strict_chain/' /etc/proxychains4.conf
+sudo sed -i 's/#dynamic_chain/dynamic_chain/' /etc/proxychains4.conf
 
-# 2. Advanced Engines
-echo -e "\e[1;33m[*] Downloading Search Engines & Mirror Scrapers...\e[0m"
+# 3. Start Tor Service
+sudo service tor restart
+
+# 4. Install OSINT Engines
 pip install colorama requests[socks] holehe maigret social-analyzer --break-system-packages --ignore-installed
 
-# 3. Path & Permissions
 mkdir -p /root/osint/reports
-chmod +x khalid-osint.py
-
-echo -e "\e[1;34m[!] Framework Taiyar! Ab 'python3 khalid-osint.py' chalaein.\e[0m"
+echo -e "\e[1;34m[!] Setup Complete! Tor Service is now running in background.\e[0m"
