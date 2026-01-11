accab7f40085e2b2373b185c66ac3fd72c45df37
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-11 19:04:03 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 # Developer: Khalid Husain (@khalidhusain786)
-# Auto-Installer & System Fixer
+# Ultimate No-Error Binary Setup
 
-echo -e "\e[1;32m[*] Khalid OSINT: Fixing Environment & Installing Tools...\e[0m"
+echo -e "\e[1;32m[*] Fast Setup Starting... No PIP Errors anymore!\e[0m"
 
-# Folder creation
+# Basic folders
 mkdir -p reports
+mkdir -p tools
 
-# System Dependencies
-sudo apt update && sudo apt install -y python3 python3-pip python3-venv git curl
+# System tools install (Fast mode)
+sudo apt update
+sudo apt install -y python3-pip python3-full git curl
 
-# Virtual Environment Setup (Error Bypass)
-python3 -m venv venv
-source venv/bin/activate
-
-# Force Install all tools
-pip install --upgrade pip
-pip install colorama requests phonenumbers holehe maigret haveibeenpwned photon-cli fpdf --break-system-packages --ignore-installed
-
-# Sherlock setup
-if [ ! -d "$HOME/sherlock" ]; then
-    git clone https://github.com/sherlock-project/sherlock.git $HOME/sherlock
-    cd $HOME/sherlock && pip install -r requirements.txt && cd -
+# Downloading Sherlock & Maigret directly (No PIP install needed)
+cd tools
+if [ ! -d "sherlock" ]; then
+    git clone https://github.com/sherlock-project/sherlock.git
+    python3 -m pip install -r sherlock/requirements.txt --break-system-packages
 fi
 
-echo -e "\e[1;34m[!] Setup Done! Ab bas 'python3 khalid-osint.py' chalao, activation auto hogi.\e[0m"
+if [ ! -d "maigret" ]; then
+    git clone https://github.com/soxoj/maigret.git
+    python3 -m pip install -r maigret/requirements.txt --break-system-packages
+fi
+cd ..
+
+# Installing core essentials for the main script
+python3 -m pip install colorama requests phonenumbers fpdf --break-system-packages
+
+chmod +x khalid-osint.py
+echo -e "\e[1;34m[!] ALL TOOLS READY! Ab seedhe chalao.\e[0m"
