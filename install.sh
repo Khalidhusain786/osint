04e8562ce4f6fefc7fa0596130c09890c759800f
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-12 07:46:07 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 export PIP_BREAK_SYSTEM_PACKAGES=1
 mkdir -p reports
-echo -e "\e[34m[*] Installing OSINT Tools & Configuring Paths...\e[0m"
+echo -e "\e[34m[*] Installing OSINT Tools & HTML Cleaner...\e[0m"
 
-# Installing required tools
-python3 -m pip install --user colorama requests[socks] holehe sherlock-project maigret blackbird photon phoneinfoga
+# Installing required tools and HTML parser
+python3 -m pip install --user colorama requests[socks] holehe sherlock-project maigret blackbird photon phoneinfoga beautifulsoup4
 
-# Linking binaries for instant access
+# Linking binaries
 sudo ln -sf ~/.local/bin/sherlock /usr/local/bin/sherlock
 sudo ln -sf ~/.local/bin/holehe /usr/local/bin/holehe
 sudo ln -sf ~/.local/bin/maigret /usr/local/bin/maigret
 sudo ln -sf ~/.local/bin/blackbird /usr/local/bin/blackbird
 sudo ln -sf ~/.local/bin/phoneinfoga /usr/local/bin/phoneinfoga
 
-echo -e "\e[32m[✔] Setup Complete. Run: python3 khalid-osint.py\e[0m"
+echo -e "\e[32m[✔] Setup Ready!\e[0m"
