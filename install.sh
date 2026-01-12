8d0df04c4d2421fb2c08e5e221dc1112bf93874b
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-12 05:15:06 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-echo -e "\e[1;32m[*] Khalid-OSINT Master Setup Shuru Ho Raha Hai...\e[0m"
+echo -e "\e[1;32m[*] Khalid-OSINT Master Setup Shuru...\e[0m"
 
-# System updates aur essential tools
+# 1. System Dependencies
 sudo apt update && sudo apt install tor proxychains4 python3-pip curl git exiftool nodejs npm googler -y
 
-# Telegram aur OSINT Libraries
-pip install colorama requests[socks] telethon holehe maigret sherlock socialscan instaloader --break-system-packages
+# 2. Python Tools (Telegram, Social, Email)
+pip install colorama requests[socks] telethon holehe maigret sherlock socialscan --break-system-packages
 
-# Social-Analyzer aur PhoneInfoga Setup
+# 3. Heavy Engines (Social Analyzer & PhoneInfoga)
 sudo npm install -g social-analyzer
 curl -sSL https://raw.githubusercontent.com/sundowndev/phoneinfoga/master/support/install | bash
 sudo mv ./phoneinfoga /usr/local/bin/
 
-# Directory aur Services
+# 4. Finalizing
 mkdir -p reports
 sudo service tor restart
-
-echo -e "\e[1;34m[!] Setup Complete! Ab bas 'python3 khalid-osint.py' chalayein.\e[0m"
+echo -e "\e[1;34m[!] Setup Complete. Ab 'python3 khalid-osint.py' chalayein.\e[0m"
