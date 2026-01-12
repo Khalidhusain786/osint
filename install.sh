8640bab0fb8792ff38856edb43ed094c7c2f0792
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-12 08:02:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 export PIP_BREAK_SYSTEM_PACKAGES=1
 mkdir -p reports
-echo -e "\e[34m[*] Configuring Final OSINT Suite...\e[0m"
+echo -e "\e[34m[*] Fixing Tools Path & Installing Cleaner...\e[0m"
 
-# Dependencies
+# Sabhi tools ko install aur link karna
 python3 -m pip install --user colorama requests beautifulsoup4 holehe sherlock-project maigret
 
-# Linking binaries for immediate use
+# PATH linking (Taki 'not found' error na aaye)
 sudo ln -sf ~/.local/bin/sherlock /usr/local/bin/sherlock
 sudo ln -sf ~/.local/bin/holehe /usr/local/bin/holehe
 sudo ln -sf ~/.local/bin/maigret /usr/local/bin/maigret
 
-echo -e "\e[32m[✔] Setup Ready! Run: python3 khalid-osint.py\e[0m"
+echo -e "\e[32m[✔] Sab kuch set hai! Ab python3 khalid-osint.py chalayein.\e[0m"
