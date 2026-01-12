904244cff0217ad21e6c465c4089ac8e2079de8f
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-12 07:53:58 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 export PIP_BREAK_SYSTEM_PACKAGES=1
 mkdir -p reports
-echo -e "\e[34m[*] Installing OSINT Suite & Auto-Submit Tools...\e[0m"
+echo -e "\e[34m[*] Installing OSINT Suite & Auto-Submit Engine...\e[0m"
 
-# Required dependencies
-python3 -m pip install --user colorama requests[socks] holehe sherlock-project maigret blackbird photon phoneinfoga beautifulsoup4
+# dependencies install karna
+python3 -m pip install --user colorama requests[socks] holehe sherlock-project maigret blackbird beautifulsoup4
 
-# System Path Linking
+# Sabhi tools ko path se link karna
 sudo ln -sf ~/.local/bin/sherlock /usr/local/bin/sherlock
 sudo ln -sf ~/.local/bin/holehe /usr/local/bin/holehe
 sudo ln -sf ~/.local/bin/maigret /usr/local/bin/maigret
 sudo ln -sf ~/.local/bin/blackbird /usr/local/bin/blackbird
-sudo ln -sf ~/.local/bin/phoneinfoga /usr/local/bin/phoneinfoga
 
 echo -e "\e[32m[✔] Setup Ready! Saare tools link ho chuke hain.\e[0m"
