fb832e1c39c5322837edc1a419e12636fe1b9360
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-12 08:09:36 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 export PIP_BREAK_SYSTEM_PACKAGES=1
-mkdir -p reports
-echo -e "\e[34m[*] Fixing Tools Path & Installing Cleaner...\e[0m"
+mkdir -p reports tools
+echo -e "\e[34m[*] Installing Social Analyzer & 30+ OSINT Tools...\e[0m"
 
-# Sabhi tools ko install aur link karna
-python3 -m pip install --user colorama requests beautifulsoup4 holehe sherlock-project maigret
+# Core Tools Installation
+python3 -m pip install --user colorama requests beautifulsoup4 holehe sherlock-project maigret blackbird photon phoneinfoga social-analyzer ghunt mosint ignorant-osint
 
-# PATH linking (Taki 'not found' error na aaye)
+# Linking Binaries (Fixes 'Not Found' error)
 sudo ln -sf ~/.local/bin/sherlock /usr/local/bin/sherlock
 sudo ln -sf ~/.local/bin/holehe /usr/local/bin/holehe
 sudo ln -sf ~/.local/bin/maigret /usr/local/bin/maigret
+sudo ln -sf ~/.local/bin/social-analyzer /usr/local/bin/social-analyzer
+sudo ln -sf ~/.local/bin/phoneinfoga /usr/local/bin/phoneinfoga
 
-echo -e "\e[32m[✔] Sab kuch set hai! Ab python3 khalid-osint.py chalayein.\e[0m"
+# Phishing & Tracking Tools
+git clone https://github.com/thewhiteh4t/seeker.git tools/seeker 2>/dev/null
+git clone https://github.com/htr-tech/zphisher.git tools/zphisher 2>/dev/null
+git clone https://github.com/Moham3dRiahi/Th3inspector.git tools/Th3inspector 2>/dev/null
+
+echo -e "\e[32m[✔] Social Analyzer and All Tools Ready!\e[0m"
