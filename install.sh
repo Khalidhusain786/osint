9ae67e00dfbd4ae52bf018d0411543cdb73e624f
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-12 04:58:30 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-echo -e "\e[1;32m[*] Installing Khalid Global-OSINT Framework...\e[0m"
+echo -e "\e[1;32m[*] Installing Khalid Deep-Data OSINT Framework...\e[0m"
 
-# 1. System & Tor Setup
-sudo apt update && sudo apt install tor proxychains4 python3-pip curl git lynx googler -y
-sudo service tor restart
+# Basic Tools
+sudo apt update && sudo apt install tor proxychains4 python3-pip -y
 
-# 2. Python Power-Tools
-pip install colorama requests[socks] telethon holehe maigret sherlock socialscan instaloader --break-system-packages
+# Libraries
+pip install colorama requests[socks] telethon holehe maigret --break-system-packages
 
-# 3. Phone & Meta Tools
+# Tools
 curl -sSL https://raw.githubusercontent.com/sundowndev/phoneinfoga/master/support/install | bash
 sudo mv ./phoneinfoga /usr/local/bin/
 
 mkdir -p reports
-echo -e "\e[1;34m[!] All layers (Surface, Deep, Dark) are ready.\e[0m"
+echo -e "\e[1;34m[!] Setup Ready. 'khalid_session' create hoga run karne par.\e[0m"
