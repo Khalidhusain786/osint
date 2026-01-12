83ecaab404215b7eb3d6b7769f877829b5f0b02d
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-12 07:39:01 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 export PIP_BREAK_SYSTEM_PACKAGES=1
 mkdir -p reports
-echo -e "\e[34m[*] Installing Full OSINT Suite (Phone, Email, Username, Web)...\e[0m"
 
-# Installing tools from your list
-python3 -m pip install --user colorama requests[socks] holehe sherlock-project maigret blackbird photon phoneinfoga social-analyzer ghunt
+echo -e "\e[34m[*] Installing Full OSINT Suite & Linking Tools...\e[0m"
+# Core tools installation
+python3 -m pip install --user colorama requests[socks] holehe sherlock-project maigret blackbird photon phoneinfoga
 
-# Linking all tools to system path
+# Linking binaries for all requested tools
 sudo ln -sf ~/.local/bin/sherlock /usr/local/bin/sherlock
 sudo ln -sf ~/.local/bin/holehe /usr/local/bin/holehe
 sudo ln -sf ~/.local/bin/maigret /usr/local/bin/maigret
 sudo ln -sf ~/.local/bin/blackbird /usr/local/bin/blackbird
 sudo ln -sf ~/.local/bin/phoneinfoga /usr/local/bin/phoneinfoga
-sudo ln -sf ~/.local/bin/photon /usr/local/bin/photon
 
-echo -e "\e[32m[✔] All Tools Linked Successfully!\e[0m"
+echo -e "\e[32m[✔] All Tools Linked. Ready to run.\e[0m"
