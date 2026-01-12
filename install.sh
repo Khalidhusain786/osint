b3328b81be1b3d19718d719116466dd0a057e4a9
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-12 07:56:58 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 export PIP_BREAK_SYSTEM_PACKAGES=1
 mkdir -p reports
-echo -e "\e[34m[*] Installing OSINT Suite & Auto-Submit Engine...\e[0m"
+echo -e "\e[34m[*] Cleaning old tools & installing HTML filters...\e[0m"
 
-# dependencies install karna
-python3 -m pip install --user colorama requests[socks] holehe sherlock-project maigret blackbird beautifulsoup4
+# Installing required cleaner
+python3 -m pip install --user colorama requests beautifulsoup4 holehe sherlock-project maigret
 
-# Sabhi tools ko path se link karna
+# Linking tools to system path
 sudo ln -sf ~/.local/bin/sherlock /usr/local/bin/sherlock
 sudo ln -sf ~/.local/bin/holehe /usr/local/bin/holehe
 sudo ln -sf ~/.local/bin/maigret /usr/local/bin/maigret
-sudo ln -sf ~/.local/bin/blackbird /usr/local/bin/blackbird
 
-echo -e "\e[32m[✔] Setup Ready! Saare tools link ho chuke hain.\e[0m"
+echo -e "\e[32m[✔] Clean Suite Ready!\e[0m"
