a12886d3be32607ca246ac5931b65a2c844c4c9a
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-12 07:43:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 export PIP_BREAK_SYSTEM_PACKAGES=1
 mkdir -p reports
-echo -e "\e[34m[*] Installing Tools & Linking Paths...\e[0m"
+echo -e "\e[34m[*] Installing OSINT Tools & Configuring Paths...\e[0m"
+
+# Installing required tools
 python3 -m pip install --user colorama requests[socks] holehe sherlock-project maigret blackbird photon phoneinfoga
+
+# Linking binaries for instant access
 sudo ln -sf ~/.local/bin/sherlock /usr/local/bin/sherlock
 sudo ln -sf ~/.local/bin/holehe /usr/local/bin/holehe
 sudo ln -sf ~/.local/bin/maigret /usr/local/bin/maigret
 sudo ln -sf ~/.local/bin/blackbird /usr/local/bin/blackbird
 sudo ln -sf ~/.local/bin/phoneinfoga /usr/local/bin/phoneinfoga
-echo -e "\e[32m[✔] All Tools Linked!\e[0m"
+
+echo -e "\e[32m[✔] Setup Complete. Run: python3 khalid-osint.py\e[0m"
